726edc9eb0129bebe2e935538d17ab9167d86744
jaestrada40 <javiera.estradag@gmail.com>
2023-11-04 17:22:40 +0000

    Add Backend config

--- 1-database/terraform.tf
+++ 1-database/terraform.tf
@@ -1,10 +1,11 @@
 terraform {
-  # backend "s3" {
-  #   bucket = ""
-  #   key    = "database/terraform.tfstate"
-  #   region = "us-east-1"
-  #   dynamodb_table = "" 
-  # }
+  backend "s3" {
+    #BUCKET tiene que cambier
+    bucket = "teraform-backend-sandbox-107"
+    key    = "sankdbox8/database/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "" 
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
